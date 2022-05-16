4a39805c0a4a4e6460ecacbdcc0597fa6e383f27
JeromeAntefar <62554856+AntoJerome11@users.noreply.github.com>
2022-05-16 23:24:30 +0400

    Add project files.

---
+++ InspectionAPI/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using InspectionAPI;
+
+
+namespace InspectionAPI.Data
+{
+    public class DataContext : DbContext
+    {
+       public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Inspection> Inspections { get; set; }
+
+        public DbSet<Status> Statuses{ get; set; }
+
+        public DbSet<InspectionAPI.InspectionType>? InspectionType { get; set; }
+    }
+}
+ 
